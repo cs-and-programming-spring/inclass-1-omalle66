23a7cac529de20d33b9e260d9f63ffce678c7484
Connor O'Malley <omalle66@rowan.edu>
2019-01-22 19:33:12 -0500

    I changed the name and email

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 
 
 /***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
+* Name: Connor O'Malley
+* Email: omalle66@students.rowan.edu
 ***********************************************
 * Purpose:
 *  This program calculates area of circle
